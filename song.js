bf662223b1790413f9105910ed0a0df5b0adbe66
zhouwenbin <zhou.wenbin.x@gmail.com>
2019-01-07 23:39:50 +0900

    homework1 submit

---
+++ song.js
@@ -0,0 +1,36 @@
+
+// this is my first homework for the online course provided by pirple
+
+
+var artist = "Queen"
+var genre = "rock"
+var memberNumber = 4
+var vocal = "Freddie Mercury"
+var issinglesinger = false
+var isenglishband = true
+
+// this is an object
+var Membernames ={
+	1:"Brian May",
+	2:"Freddie Mercury",
+	3:"Roger Taylor" 
+} 
+
+//array data
+var memberarray=["Brian May","Freddie Mercury","Roger Taylor"]
+
+
+
+console.log(artist);
+console.log(genre);
+console.log(vocal);
+console.log(memberNumber);
+console.log (isenglishband);
+console.log(issinglesinger);
+console.log(Membernames[1]);
+console.log(memberarray);
+
+/*
+In the codes above, we print out all those values defined
+
+*/
